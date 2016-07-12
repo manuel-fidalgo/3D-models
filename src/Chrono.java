ee6cb4e087f4b48e8485ca7cf92fc7246a766a18
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-07-12 11:20:40 +0200

    Added chrono for the delays

---
+++ src/Chrono.java
@@ -0,0 +1,25 @@
+
+public class Chrono {
+	
+	
+	public long DELAY;
+	public long oldTime;
+	public long newTime;
+	
+	public Chrono(long del){
+		this.DELAY = del;
+		this.oldTime = getMills();
+	}
+	public boolean isDelay(){
+		this.newTime = System.currentTimeMillis();
+		if(newTime - oldTime >= DELAY){
+			oldTime = newTime;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public static long getMills(){
+		return System.currentTimeMillis();
+	}
+}
